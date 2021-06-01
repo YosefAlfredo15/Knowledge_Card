275db7ebd69961c66f98bd6869b1522fb8860c58
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2021-06-01 23:23:57 +0700

    Add files via upload

---
+++ sains.js
@@ -0,0 +1,102 @@
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+
+          resultsContainer.innerHTML = "Selamat, Jawaban Anda Benar! (silahkan kembali ke menu awal)"
+          document.getElementById("back").innerHTML = '<form action="menu.html"><button class="button button1">Go Back</button>' 
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+
+          resultsContainer.innerHTML = "Jawaban anda Salah. Jawaban Yang benar adalah Paru-Paru. (Silahkan Mengambil kartu tantangan)"
+          document.getElementById("back").innerHTML = '<a href="Challenge.html" target="_random"><img class ="tengah" src="D:/Knowledge Card/foto/cd.png" style="width:200px;height:250px;">'
+        }
+      });
+  
+      // // show number of correct answers out of total
+      // resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Manusia bernapas menggunakan?",
+        answers: {
+          a: "Insang",
+          b: "Trakea", 
+          c: "Paru-Paru",
+          d: "Hidung",
+          
+        },
+        correctAnswer: "c"
+      },
+
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
